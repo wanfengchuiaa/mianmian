bbd23fda32bbc5d00fefabf2003667e435105057
dabazi <1051364728@qq.com>
2022-08-13 08:31:00 +0800

    完成用户功能

--- src/module-manage/components/user-add.vue
+++ src/module-manage/components/user-add.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="add-form">
-    <el-dialog :title="text+pageTitle" :visible.sync="dialogFormVisible">
+    <!-- :title="text + pageTitle" -->
+    <el-dialog title="创建用户" :visible="dialogFormVisible" @close="close">
       <el-form
         :rules="ruleInline"
         ref="dataForm"
         :model="formBase"
         label-position="left"
         label-width="120px"
-        style="width: 400px; margin-left:120px;"
+        style="width: 400px; margin-left: 120px"
       >
-
         <el-form-item :label="$t('table.username')" prop="username">
           <el-input v-model="formBase.username"></el-input>
         </el-form-item>
@@ -19,7 +19,7 @@
         <el-form-item
           :label="$t('table.paddword')"
           prop="password"
-          v-if="formBase.password!=undefined"
+          v-if="formBase.password != undefined"
         >
           <el-input v-model="formBase.password"></el-input>
         </el-form-item>
@@ -29,10 +29,13 @@
           <el-input v-model="formBase.role"></el-input>
         </el-form-item>
         <!-- 权限组 -->
-        <el-form-item :label="$t('table.permissionUser')" prop="permission_group_id">
+        <el-form-item
+          :label="$t('table.permissionUser')"
+          prop="permission_group_id"
+        >
           <el-select class="filter-item" v-model="formBase.permission_group_id">
             <el-option
-              v-for="item in PermissionGroupsList"
+              v-for="item in qxTitle"
               :value="item.id"
               :key="item.key"
               :label="item.title"
@@ -44,100 +47,106 @@
           <el-input v-model="formBase.phone"></el-input>
         </el-form-item>
 
-        <!-- 头像上传下一个版本再做 -->
-        <!-- <el-form-item :label="$t('table.avatar')" prop="avatar">
-            <el-upload
-              class="upload-demo"
-              :action="importFileUrl"
-              :on-change="handleChange"
-              :file-list="fileList" accept="image/jpeg,image/gif,image/png,image/bmp"
-              :before-upload="beforeAvatarUpload">
-              <el-button size="small" type="primary">点击上传</el-button>
-              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-            </el-upload>
-        </el-form-item>-->
         <el-form-item :label="$t('table.introduction')">
           <el-input
             type="textarea"
-            :autosize="{ minRows: 2, maxRows: 4}"
+            :autosize="{ minRows: 2, maxRows: 4 }"
             placeholder="Please input"
             v-model="formBase.introduction"
           ></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="handleClose">{{$t('table.cancel')}}</el-button>
-        <el-button type="primary" @click="createData">{{$t('table.confirm')}}</el-button>
+        <el-button @click="close">{{ $t("table.cancel") }}</el-button>
+        <el-button type="primary" @click="createData">{{
+          $t("table.confirm")
+        }}</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { detail, update, add } from '@/api/base/users'
+import { detail, update, add } from "@/api/base/users";
+import { simple } from "@/api/base/permissions";
+
 export default {
-  name: 'usersAdd',
-  props: [
-    'text',
-    'pageTitle',
-    'PermissionGroupsList',
-    'formBase',
-    'ruleInline'
-  ],
-  data () {
-    return {
-      dialogFormVisible: false
-      // fileList: [],
-      // importFileUrl: 'https://jsonplaceholder.typicode.com/posts/',
-    }
+  name: "usersAdd",
+  props: {
+    tableList: {
+      type: Array,
+    },
+    qxTitle: {
+      type: Array,
+    },
+    userId: {
+      type: Object,
+    },
+    formData: {
+      type: Object,
+    },
   },
-  computed: {},
+  data() {
+    return {
+      dialogFormVisible: false,
+      formBase: {
+        username: "",
+        email: "",
+        password: "",
+        role: "",
+        permission_group_id: "",
+        phone: "",
+        introduction: "",
+      },
+      ruleInline: {
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+        ],
+        email: [{ required: true, message: "邮箱不能为空", trigger: "blur" }],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+        ],
+      },
+      List: [],
+    };
+  },
   methods: {
     // 弹层显示
-    dialogFormV () {
-      this.dialogFormVisible = true
+    dialogFormV() {
+      this.dialogFormVisible = true;
     },
-    // 弹层隐藏
-    dialogFormH () {
-      this.dialogFormVisible = false
+
+    close() {
+      this.dialogFormVisible = false;
+      this.$refs.dataForm.resetFields();
+      this.formBase = {
+        username: "",
+        email: "",
+        password: "",
+        role: "",
+        permission_group_id: "",
+        phone: "",
+        introduction: "",
+      };
     },
-    // 退出
-    handleClose () {
-      this.$emit('handleCloseModal')
+
+    // 编辑
+    async editData() {
+      if (this.userId) {
+        this.formBase = this.userId;
+      }
+      this.dialogFormVisible = true;
     },
 
     // 表单提交
-    createData () {
-      this.$refs.dataForm.validate(valid => {
-        if (valid) {
-          this.$emit('handleCloseModal')
-          const data = {
-            ...this.formBase
-          }
-          if (this.formBase.id) {
-            update(data).then(() => {
-              this.$emit('newDataes', this.formBase)
-            })
-          } else {
-            add(this.formBase).then(() => {
-              this.$emit('newDataes', this.formBase)
-            })
-          }
-        } else {
-          this.$Message.error('*号为必填项!')
-        }
-      })
-    }
+    async createData() {
+      await add(this.formBase);
+      this.$message.success("新增用户成功");
+    },
   },
-  // 挂载结束
-
-  mounted: function () {},
-  // 创建完毕状态
-  created () {},
-  // 组件更新
-  updated: function () {}
-}
+};
 </script>
+
 <style>
 .el-form--label-left .el-form-item__label {
   text-align: right;
@@ -146,6 +155,6 @@ export default {
   margin-bottom: 22px;
 }
 .el-dialog__footer {
-  text-align: center
+  text-align: center;
 }
 </style>
