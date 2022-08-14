d6f608bc4a288b01d12b56d90c93e6209bcb7ae1
wl180 <wlwdsqa@163.com>
2022-08-14 11:45:38 +0800

    修改

--- src/module-manage/components/user-add.vue
+++ src/module-manage/components/user-add.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="add-form">
-    <!-- :title="text + pageTitle" -->
     <el-dialog title="创建用户" :visible="dialogFormVisible" @close="close">
       <el-form
         :rules="ruleInline"
@@ -10,43 +9,39 @@
         label-width="120px"
         style="width: 400px; margin-left: 120px"
       >
+        <!-- ------------------------- 用户名 ------------------------- -->
         <el-form-item :label="$t('table.username')" prop="username">
           <el-input v-model="formBase.username"></el-input>
         </el-form-item>
+
+        <!-- ------------------------- 邮箱 ------------------------- -->
         <el-form-item :label="$t('table.email')" prop="email">
           <el-input v-model="formBase.email"></el-input>
         </el-form-item>
-        <el-form-item
-          :label="$t('table.paddword')"
-          prop="password"
-          v-if="formBase.password != undefined"
-        >
+
+        <!-- ------------------------- 密码 ------------------------- -->
+        <el-form-item :label="$t('table.paddword')" prop="password" v-if="formBase.password != undefined">
           <el-input v-model="formBase.password"></el-input>
         </el-form-item>
 
-        <!-- 角色 -->
+        <!-- ------------------------- 角色 ------------------------- -->
         <el-form-item :label="$t('table.role')" prop="role">
           <el-input v-model="formBase.role"></el-input>
         </el-form-item>
-        <!-- 权限组 -->
-        <el-form-item
-          :label="$t('table.permissionUser')"
-          prop="permission_group_id"
-        >
-          <el-select class="filter-item" v-model="value">
-            <el-option
-              v-for="item in powerList"
-              :value="item.permission_group_title"
-              :key="item.key"
-              :label="item.title"
-            ></el-option>
+
+        <!-- ------------------------- 权限组 ------------------------- -->
+        <el-form-item :label="$t('table.permissionUser')" prop="permission_group_id">
+          <el-select class="filter-item" v-model="formBase.permission_group_id">
+            <el-option v-for="item in powerList" :value="item.id" :key="item.key" :label="item.title"></el-option>
           </el-select>
         </el-form-item>
 
+        <!-- ------------------------- 联系电话 ------------------------- -->
         <el-form-item :label="$t('table.phone')" prop="phone">
           <el-input v-model="formBase.phone"></el-input>
         </el-form-item>
 
+        <!-- ------------------------- 介绍 ------------------------- -->
         <el-form-item :label="$t('table.introduction')">
           <el-input
             type="textarea"
@@ -57,122 +52,110 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="close">{{ $t("table.cancel") }}</el-button>
-        <el-button type="primary" @click="createData">{{
-          $t("table.confirm")
-        }}</el-button>
+        <el-button @click="close">{{ $t('table.cancel') }}</el-button>
+        <el-button type="primary" @click="createData">{{ $t('table.confirm') }}</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { detail, update, add } from "@/api/base/users";
-import { simple } from "@/api/base/permissions";
+import { add, detail, update } from '@/api/base/users'
+import { simple } from '@/api/base/permissions'
 
 export default {
-  name: "usersAdd",
-  props: {
-    tableList: {
-      type: Array,
-    },
-    qxTitle: {
-      type: Array,
-    },
-    userId: {
-      type: Object,
-    },
-    formData: {
-      type: Object,
-    },
-  },
-  data() {
+  name: 'usersAdd',
+  data () {
     return {
+      userId: false,
       dialogFormVisible: false,
       formBase: {
-        username: "",
-        email: "",
-        password: "",
-        role: "",
-        permission_group_id: "",
-        phone: "",
-        introduction: "",
+        avatar: '',
+        username: '',
+        email: '',
+        password: '',
+        role: '',
+        permission_group_id: '',
+        phone: '',
+        introduction: ''
       },
       ruleInline: {
-        username: [
-          { required: true, message: "用户名不能为空", trigger: "blur" },
-        ],
-        email: [{ required: true, message: "邮箱不能为空", trigger: "blur" }],
-        password: [
-          { required: true, message: "密码不能为空", trigger: "blur" },
-        ],
+        username: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+        email: [{ required: true, message: '邮箱不能为空', trigger: 'blur' }],
+        password: [{ required: true, message: '密码不能为空', trigger: 'blur' }]
       },
-      List: [],
       powerList: [],
-      value: "",
-    };
+      value: ''
+    }
   },
-  created() {
-    this.simple();
+  created () {
+    this.simple()
   },
   methods: {
-    // 弹层显示
-    dialogFormV() {
-      this.dialogFormVisible = true;
+    dialogFormV () {
+      this.dialogFormVisible = true
     },
 
-    close() {
-      this.dialogFormVisible = false;
-      this.$refs.dataForm.resetFields();
+    close () {
+      this.userId = false
+      this.dialogFormVisible = false
+      this.$refs.dataForm.resetFields()
       this.formBase = {
-        username: "",
-        email: "",
-        password: "",
-        role: "",
-        permission_group_id: "",
-        phone: "",
-        introduction: "",
-      };
-    },
-
-    async simple() {
-      const res = await simple();
-      console.log(res.data);
-      this.powerList = res.data;
-    },
-
-    // 编辑
-    async editData() {
-      // console.log(this.userId);
-      const res = await detail(this.userId);
-      console.log(res);
-      // console.log(this.qxTitle);
-
-      this.formBase = res.data;
-
-      this.dialogFormVisible = true;
-    },
-
-    // 表单提交
-    async createData() {
-      if (this.userId) {
-        await update(this.formBase);
-      } else {
-        await add(this.formBase);
-        this.$message.success("新增用户成功");
+        avatar: '',
+        username: '',
+        email: '',
+        password: '',
+        role: '',
+        permission_group_id: '',
+        phone: '',
+        introduction: ''
       }
     },
-  },
-};
+
+    async simple () {
+      const res = await simple()
+      this.powerList = res.data
+    },
+
+    async editData (data) {
+      this.userId = true
+      const obj = Object.keys(this.formBase)
+      const res = await detail(data)
+      for (const key in res.data) {
+        if (obj.includes(key)) {
+          this.formBase[key] = res.data[key]
+        }
+      }
+      // this.formBase = res.data
+
+      this.formBase.id = res.data.id
+      this.dialogFormVisible = true
+    },
+
+    async createData () {
+      // await this.$refs.dataForm.vlitate()
+      if (this.userId) {
+        await update(this.formBase)
+        this.$message.success('修改成功')
+      } else {
+        await add(this.formBase)
+        this.$message.success('新增用户成功')
+      }
+      this.close()
+    }
+  }
+}
 </script>
 
 <style>
 .el-form--label-left .el-form-item__label {
   text-align: right;
 }
+
 .el-form-item--medium {
   margin-bottom: 22px;
 }
+
 .el-dialog__footer {
   text-align: center;
 }
--- src/module-manage/pages/users.vue
+++ src/module-manage/pages/users.vue
@@ -4,73 +4,34 @@
       <!-- ------------------- 搜索新增区域 ------------------- -->
       <el-row type="flex" justify="space-between">
         <el-col>
-          <el-input
-            style="width: 200px"
-            size="small"
-            placeholder="根据用户名搜索"
-            v-model="userName"
-          ></el-input>
-          <el-button size="small" style="margin-left: 15px" @click="clearSearch"
-          >清空
-          </el-button
-          >
-          <el-button size="small" type="primary" @click="tableData1"
-          >搜索
-          </el-button
-          >
+          <el-input style="width: 200px" size="small" placeholder="根据用户名搜索" v-model="userName"></el-input>
+          <el-button size="small" style="margin-left: 15px" @click="clearSearch">清空</el-button>
+          <el-button size="small" type="primary" @click="tableData1">搜索</el-button>
         </el-col>
-        <el-col></el-col>
+        <el-col> </el-col>
         <el-row>
           <el-col>
-            <el-button
-              size="small"
-              icon="el-icon-edit"
-              type="success"
-              @click="addUserDialog"
-            >新增用户
-            </el-button
+            <el-button size="small" icon="el-icon-edit" type="success" @click="$refs.addUser.dialogFormV()"
+              >新增用户</el-button
             >
           </el-col>
         </el-row>
       </el-row>
 
       <!-- ------------------- 消息提示 ------------------- -->
-      <el-alert
-        style="margin: 20px 0"
-        title="共xx条记录"
-        type="info"
-        show-icon
-        :closable="false"
-      >
-      </el-alert>
+      <el-alert style="margin: 20px 0" :title="`共${counts}条记录`" type="info" show-icon :closable="false"> </el-alert>
 
       <!-- ------------------- 表格数据区域 ------------------- -->
       <el-table :data="tableList" stripe style="width: 100%">
-        <el-table-column align="center" prop="id" label="序号" width="180">
-        </el-table-column>
-        <el-table-column align="center" prop="email" label="邮箱" width="180">
-        </el-table-column>
-        <el-table-column align="center" prop="phone" label="联系电话">
-        </el-table-column>
-        <el-table-column align="center" prop="username" label="用户名">
-        </el-table-column>
-        <el-table-column
-          align="center"
-          prop="permission_group_title"
-          label="权限组名称"
-        >
-        </el-table-column>
-        <el-table-column align="center" prop="role" label="角色">
-        </el-table-column>
+        <el-table-column align="center" prop="id" label="序号" width="180"> </el-table-column>
+        <el-table-column align="center" prop="email" label="邮箱" width="180"> </el-table-column>
+        <el-table-column align="center" prop="phone" label="联系电话"> </el-table-column>
+        <el-table-column align="center" prop="username" label="用户名"> </el-table-column>
+        <el-table-column align="center" prop="permission_group_title" label="权限组名称"> </el-table-column>
+        <el-table-column align="center" prop="role" label="角色"> </el-table-column>
         <el-table-column align="center" label="操作">
           <template v-slot="{ row }">
-            <el-button
-              icon="el-icon-edit"
-              type="primary"
-              plain
-              circle
-              @click="handleEdit(row)"
-            ></el-button>
+            <el-button icon="el-icon-edit" type="primary" plain circle @click="$refs.addUser.editData(row)"></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -91,14 +52,8 @@
       </el-row>
     </el-card>
 
-    <!-- ------------------- 新增用户弹层 ------------------- -->
-    <usersAdd
-      :formData="formData"
-      :userId="userId"
-      :qxTitle="qxTitle"
-      :tableList="tableList"
-      ref="addUser"
-    ></usersAdd>
+    <!-- ------------------- 新增和编辑用户共用弹层 ------------------- -->
+    <usersAdd ref="addUser"></usersAdd>
   </div>
 </template>
 
@@ -106,63 +61,50 @@
 import PageTool from '../components/page-tool.vue'
 import usersAdd from '../components/user-add.vue'
 import { list } from '@/api/base/users'
-import { simple } from '@/api/base/permissions'
-
 export default {
-
   components: {
     PageTool,
-    usersAdd
+    usersAdd,
   },
-  data () {
+  data() {
     return {
-      formData: {},
       tableList: [],
       userName: '',
-      qxTitle: [],
-      userId: {},
       page: 1,
       pagesize: 10,
-      counts: 0
+      counts: 0,
     }
   },
 
-  created () {
+  created() {
     this.tableData1()
   },
 
   methods: {
-    handleSizeChange (val) {
-      this.pagesize = val
-      this.tableData1()
-    },
-    handleCurrentChange (val) {
-      this.page = val
-      this.tableData1()
-    },
-    clearSearch () {
+    clearSearch() {
       this.userName = ''
       this.tableData1()
     },
-    async handleEdit (data) {
-      this.userId = data
-      this.$refs.addUser.editData()
-    },
-    async tableData1 () {
+
+    async tableData1() {
       const res = await list({
         username: this.userName,
         page: this.page,
-        pagesize: this.pagesize
+        pagesize: this.pagesize,
       })
       this.counts = res.data.counts
       this.tableList = res.data.list
     },
-    async addUserDialog () {
-      const res = await simple()
-      this.qxTitle = res.data
-      this.$refs.addUser.dialogFormV()
-    }
-  }
+
+    handleSizeChange(val) {
+      this.pagesize = val
+      this.tableData1()
+    },
+    handleCurrentChange(val) {
+      this.page = val
+      this.tableData1()
+    },
+  },
 }
 </script>
 
